de36d93e2c38afae3f7b35c4023c962d316a8461
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-09 20:53:11 -0400

    extra spaces deletion

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ app.set('view engine', 'ejs')
 
 // Mount routes
 
-// app.get('/', function(req, res) {
-//   res.send('<h1>Hello Express</h1>')
-// })
-
 app.get('/home', function(req, res) {
   res.render('home')
 })
